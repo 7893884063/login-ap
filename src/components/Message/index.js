d65c07b2bbfa2142b4c08856718867698c816e44
7893884063 <anilkumar1003200@gmail.com>
2023-03-25 16:30:07 +0000

    login App

---
+++ src/components/Message/index.js
@@ -0,0 +1,12 @@
+// Write your code here
+// Write your code here
+import './index.css'
+
+const Message = props => {
+  const {IsloggedIn} = props
+  const message = IsloggedIn ? 'Welcome User' : 'Please Login'
+
+  return <h1 className="message">{message}</h1>
+}
+
+export default Message
